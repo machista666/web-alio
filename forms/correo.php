728aa8ecbe064584b47ac5b7c6c94a369fbe5921
machista666 <isaianayacastro0@gmail.com>
2025-01-29 13:03:07 -0600

    Segundo Commit

--- forms/correo.php
+++ forms/correo.php
@@ -19,7 +19,7 @@ $message = $_POST['message'];
 $to = 'sistemas@alio.mx'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contacto de Sitio web:  $subject";
 $email_body = "Gracias por ponerte en contacto con Alio.\n\n"."En breve te estaremos contactando con más detalles.";
-$headers = "From: rodriguezdaniel@alio.mx\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: contacto@alio.mx\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
